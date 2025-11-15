f45d47683bb9568f32366a193464bcf96cefb7fe
samroska <samantha.roska@gmail.com>
2025-11-15 16:05:52 -0700

    fix: updates

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ app.add_middleware(
 
 # app.add_middleware(
 #     CORSMiddleware,
-#     allow_origins=["http://localhost:5174"],
+#     allow_origins=["http://localhost:5173"],
 #     allow_credentials=True,
 #     allow_methods=["GET", "POST", "OPTIONS"],
 #     allow_headers=["*"],
